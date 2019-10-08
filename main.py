8487f91e861eb04094be8cdd8e1f1d36a918380e
Trường Nguyễn <35923241+dinmythng@users.noreply.github.com>
2019-10-08 18:12:25 +0700

    Revert "Update main.py"
    This reverts commit dccca91dcdc37f0139d4d9a905e407ea71556892, reversing
    changes made to 13ba465f99cc518f01148138ef9fb541fa90a5a1.

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-﻿import pygame as pg
+import pygame as pg
 import tkinter as tk
 from tkinter import filedialog
 import random as rd
 import sys
 import queue
 
-
-# Global Var
 scale = 1
 
 white = (255, 255, 255, 255)
@@ -25,19 +23,6 @@ yellow = (230, 230, 0, 255)
 gray = (169, 169, 169, 255)
 black = (90, 90, 90, 255)
 
-brown = (128, 0, 0, 255)
-tan = (210, 180, 140)
-
-run = True
-run_sim = False
-start_pos = []
-end_pos = []
-polygon_set = []
-no_input = True
-cost = 0
-pickup_points = []
-sub_path = []
-
 
 class Node:
     def __init__(self, point=None, back_node=None):
@@ -52,7 +37,10 @@ class Node:
         return self.point[0] == other.point[0] and self.point[1] == other.point[1]
 
 
-# Pathfinding functions
+def is_out_of_bound(point):
+    return point[0] > 1280 - 1 or point[1] > 720 - 1
+
+
 def breadth_first(display, start_posi, end_posi, max_x, max_y):
     start = Node(start_posi, None)
     end = Node(end_posi, None)
@@ -68,7 +56,6 @@ def breadth_first(display, start_posi, end_posi, max_x, max_y):
         passed_node.append(curr_node)
         to_draw_p.append(curr_node)
 
-<<<<<<< HEAD
         if curr_node == end:
             path = []
             node = curr_node
@@ -129,8 +116,6 @@ def breadth_first(display, start_posi, end_posi, max_x, max_y):
             frontier.put(node)
             to_draw_f.append(node)
 
-=======
->>>>>>> 4f25763851ed527a01da40adf496abde7f09306a
         for node in to_draw_p:
             draw_cell(display, node.point, green)
         to_draw_p.clear()
@@ -170,13 +155,10 @@ def find_path_astar(display, start_posi, end_posi, max_x, max_y):
                 path.append(node.point)
                 node = node.back_node
             for cell in path:
-                pcolor = pg.Surface.get_at(display, (cell[0] * scale + int(scale / 2), cell[1] * scale + int(scale / 2)))
-                if pcolor == blue or pcolor == brown or pcolor == tan or pcolor == red:
-                    continue
                 draw_cell(display, cell, lightblue)
             draw_cell(display, start_pos, blue)
             draw_cell(display, end_pos, red)
-            return curr_node.f, path[::-1]
+            return curr_node.f
 
         next_nodes = []
         for next in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
@@ -195,17 +177,17 @@ def find_path_astar(display, start_posi, end_posi, max_x, max_y):
             # Xét không đi xéo qua hình
             if cross:
                 x_color = pg.Surface.get_at(display, (
-                    new_node[0] * scale + int(scale / 2), curr_node.point[1] * scale + int(scale / 2)))
-                if x_color == purple or x_color == black or x_color == yellow or x_color == pink or x_color == coral or x_color == orange:
+                new_node[0] * scale + int(scale / 2), curr_node.point[1] * scale + int(scale / 2)))
+                if x_color == purple or p_color == black or p_color == yellow or p_color == pink or p_color == coral or p_color == orange:
                     continue
                 y_color = pg.Surface.get_at(display, (
-                    curr_node.point[0] * scale + int(scale / 2), new_node[1] * scale + int(scale / 2)))
-                if y_color == purple or y_color == black or y_color == yellow or y_color == pink or y_color == coral or y_color == orange:
+                curr_node.point[0] * scale + int(scale / 2), new_node[1] * scale + int(scale / 2)))
+                if y_color == purple or p_color == black or p_color == yellow or p_color == pink or p_color == coral or p_color == orange:
                     continue
 
             p_color = pg.Surface.get_at(display,
                                         (new_node[0] * scale + int(scale / 2), new_node[1] * scale + int(scale / 2)))
-            if p_color == purple or p_color == black or p_color == yellow or p_color == pink or p_color == coral or p_color == orange:
+            if p_color == purple or p_color == black or p_color == yellow or p_color == pink:
                 continue
 
             next_node = Node(new_node, curr_node)
@@ -228,83 +210,14 @@ def find_path_astar(display, start_posi, end_posi, max_x, max_y):
             to_draw_f.append(node)
 
         for node in to_draw_p:
-            pcolor = pg.Surface.get_at(display, (node.point[0] * scale + int(scale / 2), node.point[1] * scale + int(scale / 2)))
-            if pcolor == blue or pcolor == brown or pcolor == lightblue or pcolor == tan:
-                continue
             draw_cell(display, node.point, green)
         to_draw_p.clear()
 
         for node in to_draw_f:
-            pcolor = pg.Surface.get_at(display,
-                                       (node.point[0] * scale + int(scale / 2), node.point[1] * scale + int(scale / 2)))
-            if pcolor == blue or pcolor == brown or pcolor == lightblue or pcolor == tan:
-                continue
             draw_cell(display, node.point, steelblue)
         to_draw_f.clear()
 
         pg.display.update()
-
-    return -1, []
-
-
-def find_path():
-    if len(pickup_points) > 0:
-        cloned_pickup = pickup_points.copy()
-        final_path = []
-        final_cost = 0
-        global sub_path
-        sub_path = [start_pos]
-        curr_point = start_pos
-        while len(cloned_pickup) > 0:
-            route_costpath = []
-            clear_route(screen, int(1280 / scale), int(720 / scale))
-
-            for point in cloned_pickup:
-                sub_cost_path = find_path_astar(screen, curr_point, point, 1280 / scale, 720 / scale)
-                if sub_cost_path[0] == -1:
-                    return -1
-                route_costpath.append(sub_cost_path)
-
-            min_ind = 0
-            for i in range(0, len(route_costpath)):
-                if route_costpath[i][0] < route_costpath[min_ind][0]:
-                    min_ind = i
-
-            curr_point = cloned_pickup[min_ind]
-            cloned_pickup.pop(min_ind)
-            sub_path.append(curr_point)
-
-            final_path.append(route_costpath[min_ind][1])
-            final_cost += route_costpath[min_ind][0]
-
-            draw_cell(screen, curr_point, brown)
-
-        clear_route(screen, int(1280 / scale), int(720 / scale))
-
-        sub_pathcost = find_path_astar(screen, sub_path[len(sub_path) - 1], end_pos, 1280 / scale, 720 / scale)
-        if sub_pathcost[0] == -1:
-            return -1
-        final_path.append(sub_pathcost[1])
-        final_cost += sub_pathcost[0]
-        sub_path.append(end_pos)
-
-        clear_route(screen, int(1280 / scale), int(720 / scale))
-
-        for _path in final_path:
-            for cell in _path:
-                pcolor = pg.Surface.get_at(screen, (cell[0] * scale + int(scale / 2), cell[1] * scale + int(scale / 2)))
-                if pcolor == blue or pcolor == brown or pcolor == tan or pcolor == red:
-                    continue
-                draw_cell(screen, cell, lightblue)
-
-        return final_cost
-    else:
-        return find_path_astar(screen, start_pos, end_pos, int(1280 / scale), int(720 / scale))[0]
-
-
-# Draw + Coord functions
-def is_out_of_bound(point):
-    return point[0] > 1280 - 1 or point[1] > 720 - 1
 
 
 def flip_y(point):
@@ -318,10 +231,8 @@ def get_scale(point):
 def get_cell(x, y):
     x *= scale
     y *= scale
-    return (x + int(scale / 20) + 1, y + int(scale / 20) + 1), (
-    x - int(scale / 20) - 1 + scale, y + int(scale / 20) + 1), (
-               x - int(scale / 20) - 1 + scale, y - int(scale / 20) - 1 + scale), (
-           x + int(scale / 20) + 1, y - int(scale / 20) - 1 + scale)
+    return (x + int(scale / 20) + 1, y + int(scale / 20) + 1), (x - int(scale / 20) - 1 + scale, y + int(scale / 20) + 1), (
+        x - int(scale / 20) - 1 + scale, y - int(scale / 20) - 1 + scale), (x + int(scale / 20) + 1, y - int(scale / 20) - 1 + scale)
 
 
 def draw_grid(display):
@@ -340,7 +251,7 @@ def draw_cell(display, point, colour):
         pg.draw.polygon(display, colour, get_cell(point[0], point[1]))
 
 
-        # Vẽ 1 hình từ các đỉnh
+# Vẽ 1 hình từ các đỉnh
 def draw_shape(display, points):
     scaled = []
     if scale == 1:
@@ -385,50 +296,6 @@ def draw_shape(display, points):
     draw_grid(display)
 
 
-def reset(display):
-    display.fill(white)
-
-    for polygon in polygon_set:
-        draw_shape(display, polygon)
-
-    draw_grid(display)
-    if scale != 1:
-        draw_cell(display, start_pos, blue)
-        draw_cell(display, end_pos, red)
-        for cell in pickup_points:
-            if cell in sub_path:
-                draw_cell(display, cell, brown)
-            else:
-                draw_cell(display, cell, tan)
-    else:
-        pg.draw.circle(display, blue, start_pos, 5)
-        pg.draw.circle(display, red, end_pos, 5)
-        for cell in pickup_points:
-            if cell in sub_path:
-                pg.draw.circle(display, brown, cell, 5)
-            else:
-                pg.draw.circle(display, tan, cell, 5)
-
-    pg.display.update()
-
-def clear_route(display, max_x, max_y):
-    for i in range(0, max_x):
-        for j in range(0, max_y):
-            color = pg.Surface.get_at(display, (i * scale + int(scale / 2), j * scale + int(scale / 2)))
-            if color == steelblue or color == green or color == lightblue:
-                draw_cell(display, (i, j), white)
-
-
-# Winform functions
-def load_pick_up(string):
-    num_set = ([int(i) for i in str(string).split(',')])
-    i = 0
-    global pickup_points
-    while i < len(num_set):
-        pickup_points.append(flip_y((num_set[i], num_set[i + 1])))
-        i += 2
-
-
 def open_file():
     file_path = filedialog.askopenfilename()
     if not file_path:
@@ -466,30 +333,20 @@ def open_file():
     global run_sim
     run_sim = True
 
-    global no_input
-    no_input = False
-
     wd.destroy()
 
 
 def start_sim():
-    if e1.get():
-        global start_pos
-        start_pos = [int(i) for i in str(e1.get()).split(',')]
-    if e2.get():
-        global end_pos
-        end_pos = [int(i) for i in str(e2.get()).split(',')]
+    global start_pos
+    start_pos = [int(i) for i in str(e1.get()).split(',')]
+    global end_pos
+    end_pos = [int(i) for i in str(e2.get()).split(',')]
 
-    if e3.get():
-        size = [int(i) for i in str(e3.get()).split(',')]
-        global scale
-        scale = min(int(1280 / size[0]), int(720 / size[1]))
-        global run_sim
-        run_sim = True
-
-    if e4.get():
-        load_pick_up(e4.get())
-
+    size = [int(i) for i in str(e3.get()).split(',')]
+    global scale
+    scale = min(int(1280 / size[0]), int(720 / size[1]))
+    global run_sim
+    run_sim = True
     wd.destroy()
 
 
@@ -499,15 +356,17 @@ def end_prog():
     wd.destroy()
 
 
-# Main Function
-
 pg.init()
 
-while run:
-    sub_path.clear()
-    pickup_points.clear()
-    polygon_set.clear()
+run = True
+run_sim = False
+start_pos = []
+end_pos = []
+polygon_set = []
+no_input = True
+cost = 0
 
+while run:
     wd = tk.Tk()
     wd.title('Project 1 - Path finding')
     wd.attributes("-topmost", True)
@@ -521,26 +380,19 @@ while run:
     l3 = tk.Label(wd, text="Map size: ")
     l3.config(font=("Time new Roman", 14))
     l3.grid(row=2, column=0)
-    l3 = tk.Label(wd, text="Pick up points: ")
-    l3.config(font=("Time new Roman", 14))
-    l3.grid(row=3, column=0)
 
     e1 = tk.Entry(wd)
-    e2 = tk.Entry(wd)
-    e3 = tk.Entry(wd)
-    e4 = tk.Entry(wd)
     e1.config(font=("Time new Roman", 12))
     e1.grid(row=0, column=1)
     e1.insert(0, "10,10")
+    e2 = tk.Entry(wd)
     e2.config(font=("Time new Roman", 12))
     e2.grid(row=1, column=1)
-    e2.insert(0, "100,60")
+    e2.insert(0, "60,30")
+    e3 = tk.Entry(wd)
     e3.config(font=("Time new Roman", 12))
     e3.grid(row=2, column=1)
     e3.insert(0, "128, 72")
-    e4.config(font=("Time new Roman", 12))
-    e4.grid(row=3, column=1)
-    e4.insert(0, "23,43,65,43,77,20")
 
     b1 = tk.Button(wd, text="Start", width=15, command=start_sim)
     b1.config(font=("Time new Roman", 14))
@@ -548,9 +400,9 @@ while run:
     b2.config(font=("Time new Roman", 14))
     b3 = tk.Button(wd, text="Read from file", width=15, command=open_file)
     b3.config(font=("Time new Roman", 14))
-    b1.grid(row=5, column=0)
-    b2.grid(row=5, column=1)
-    b3.grid(row=4, column=1)
+    b1.grid(row=4, column=0)
+    b2.grid(row=4, column=1)
+    b3.grid(row=3, column=1)
 
     wd.mainloop()
 
@@ -562,8 +414,22 @@ while run:
 
     screen = pg.display.set_mode((1280, 720))
     pg.display.set_caption("Simulation")
+    screen.fill(white)
 
-    reset(screen)
+    for polygon in polygon_set:
+        draw_shape(screen, polygon)
+
+    draw_grid(screen)
+    if scale != 1:
+        draw_cell(screen, start_pos, blue)
+        draw_cell(screen, end_pos, red)
+    else:
+        pg.draw.circle(screen, blue, start_pos, 5)
+        pg.draw.circle(screen, red, end_pos, 5)
+
+    pg.display.update()
+
+    print(scale)
 
     while run_sim:
         pg.time.delay(10)
@@ -601,12 +467,8 @@ while run:
                         pg.draw.circle(screen, black, pg.mouse.get_pos(), 5)
             elif event.type == pg.KEYUP:
                 if event.key == pg.K_RETURN:
-<<<<<<< HEAD
                     #cost = find_path_astar(screen, start_pos, end_pos, 1280 / scale, 720 / scale)
                     cost = breadth_first(screen, start_pos, end_pos, 1280 / scale, 720 / scale)
-=======
-                    cost = find_path()
->>>>>>> 4f25763851ed527a01da40adf496abde7f09306a
                     run_sim = False
         pg.display.update()
 
@@ -614,19 +476,7 @@ while run:
         mess = tk.Tk()
         mess.title('Completed')
         mess.attributes("-topmost", True)
-
-        text = ""
-        if cost == -1:
-            text = "Cannot find path to target!"
-        else:
-            if len(sub_path) > 0:
-                path = [str(i) for i in sub_path]
-                s_cost = "{0:.3f}".format(cost)
-                text = "Path: %s\nTotal cost: %s" % (path, s_cost)
-            else:
-                text = "      Total cost: %s      " % ("{0:.3f}".format(cost))
-
-        l_time = tk.Label(mess, text=text)
+        l_time = tk.Label(mess, text="Total cost: " + "{0:.3f}".format(cost))
         l_time.config(font=("Time new Roman", 18))
         l_time.pack()
 
