df064cd891caf67ab082829ac6d63b0fcf631542
toanmadrid <37981186+toanmadrid@users.noreply.github.com>
2019-10-09 11:57:46 +0700

    Revert "Hoàn tất uniform_cost_search và fix đè lên hình"
    This reverts commit 59e944bf7752a2f18d1270c4d09d88eb4fa83909.

--- main.py
+++ main.py
@@ -44,7 +44,6 @@ def is_out_of_bound(point):
 def breadth_first(display, start_posi, end_posi, max_x, max_y):
     start = Node(start_posi, None)
     end = Node(end_posi, None)
-    end = Node(end_posi, None)
 
     frontier = queue.Queue()
     frontier.put(start)
@@ -95,7 +94,7 @@ def breadth_first(display, start_posi, end_posi, max_x, max_y):
 
             p_color = pg.Surface.get_at(display,
                                         (new_node[0] * scale + int(scale / 2), new_node[1] * scale + int(scale / 2)))
-            if p_color == purple or p_color == black or p_color == yellow or p_color == pink or p_color == coral or p_color == orange:
+            if p_color == purple or p_color == black or p_color == yellow or p_color == pink:
                     continue
 
             next_node = Node(new_node, curr_node)
@@ -188,7 +187,7 @@ def find_path_astar(display, start_posi, end_posi, max_x, max_y):
 
             p_color = pg.Surface.get_at(display,
                                         (new_node[0] * scale + int(scale / 2), new_node[1] * scale + int(scale / 2)))
-            if p_color == purple or p_color == black or p_color == yellow or p_color == pink or p_color == coral or p_color == orange:
+            if p_color == purple or p_color == black or p_color == yellow or p_color == pink:
                 continue
 
             next_node = Node(new_node, curr_node)
@@ -220,96 +219,6 @@ def find_path_astar(display, start_posi, end_posi, max_x, max_y):
 
         pg.display.update()
 
-
-def uniform_cost(display, start_posi, end_posi, max_x, max_y):
-    start = Node(start_posi, None)
-    end = Node(end_posi, None)
-
-    frontier = [start]
-    to_draw_f = []
-    passed_node = []
-    to_draw_p = []
-
-    while len(frontier) > 0:
-        curr_node = frontier[0]
-        curr_index = 0
-        for index, item in enumerate(frontier):
-            if item.f < curr_node.f:
-                curr_node = item
-                curr_index = index
-
-        frontier.pop(curr_index)
-        passed_node.append(curr_node)
-        to_draw_p.append(curr_node)
-
-        if curr_node == end:
-            path = []
-            node = curr_node
-            while node is not None:
-                path.append(node.point)
-                node = node.back_node
-            for cell in path:
-                draw_cell(display, cell, lightblue)
-            draw_cell(display, start_pos, blue)
-            draw_cell(display, end_pos, red)
-            return curr_node.f
-
-        next_nodes = []
-        for next in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
-            new_node = (curr_node.point[0] + next[0], curr_node.point[1] + next[1])
-            cross = False
-            if next[0] != 0 and next[1] != 0:
-                cross = True
-
-            if new_node[0] > max_x - 1 or new_node[0] < 0 or new_node[1] > max_y - 1 or new_node[1] < 0:
-                continue
-
-            if scale != 1 and is_out_of_bound(
-                    (new_node[0] * scale + int(scale / 5), new_node[1] * scale + int(scale / 5))):
-                continue
-
-            # Xét không đi xéo qua hình
-            if cross:
-                x_color = pg.Surface.get_at(display, (
-                    new_node[0] * scale + int(scale / 2), curr_node.point[1] * scale + int(scale / 2)))
-                if x_color == purple or p_color == black or p_color == yellow or p_color == pink or p_color == coral or p_color == orange:
-                    continue
-                y_color = pg.Surface.get_at(display, (
-                    curr_node.point[0] * scale + int(scale / 2), new_node[1] * scale + int(scale / 2)))
-                if y_color == purple or p_color == black or p_color == yellow or p_color == pink or p_color == coral or p_color == orange:
-                    continue
-
-            p_color = pg.Surface.get_at(display,
-                                        (new_node[0] * scale + int(scale / 2), new_node[1] * scale + int(scale / 2)))
-            if p_color == purple or p_color == black or p_color == yellow or p_color == pink or p_color == orange or p_color == coral:
-                continue
-
-            next_node = Node(new_node, curr_node)
-            next_nodes.append(next_node)
-
-        for node in next_nodes:
-            if node in passed_node:
-                continue
-
-            node.f = curr_node.f + 1
-            if cross:
-                node.f += 0.4
-
-            if node in frontier:
-                continue
-
-            frontier.append(node)
-            to_draw_f.append(node)
-
-        for node in to_draw_p:
-            draw_cell(display, node.point, green)
-        to_draw_p.clear()
-
-        for node in to_draw_f:
-            draw_cell(display, node.point, steelblue)
-        to_draw_f.clear()
-
-        pg.display.update()
 
 def flip_y(point):
     return point[0], int(719 / scale) - point[1]
@@ -560,7 +469,6 @@ while run:
                 if event.key == pg.K_RETURN:
                     #cost = find_path_astar(screen, start_pos, end_pos, 1280 / scale, 720 / scale)
                     cost = breadth_first(screen, start_pos, end_pos, 1280 / scale, 720 / scale)
-                    #cost = uniform_cost(screen, start_pos, end_pos, 1280 / scale, 720 / scale)
                     run_sim = False
         pg.display.update()
 
